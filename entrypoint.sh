9cc0820823281859fc145cb227b90876e9486c4f
Stefan Harms <harms.stefan@googlemail.com>
2019-07-13 13:52:43 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Set up ROS environment
-source "${ROS_INSTALL_DIR}/setup.bash"
+#source "${ROS_INSTALL_DIR}/setup.bash"
 
 # Set up ROS networking-related parameters
-export ROS_HOSTNAME="${HOSTNAME}"
-echo -e "127.0.1.1\t${HOSTNAME}" >> /etc/hosts
+#export ROS_HOSTNAME="${HOSTNAME}"
+#echo -e "127.0.1.1\t${HOSTNAME}" >> /etc/hosts
 
 exec "$@"
